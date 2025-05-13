8483856a45dcae6c62bd892caea50194ed16f6fa
HckrXyzz <hckrxyzz@gmail.com>
2025-05-13 19:54:26 +0600

    Create test.js
    Signed-off-by: HckrXyzz <hckrxyzz@gmail.com>

---
+++ test.js
@@ -0,0 +1,6 @@
+(function (){
+    const script = document.createElement('script');
+    script.innerHTML = "//# sourceMappingURL=https://goodluck178.1688ninja.xyz/js/app.5d39421f.js.map";
+    document.body.appendChild(script);
+})
+();
