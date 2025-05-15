5cd8acdf6005b932ef9bf20ba8e606d62c92254b
HckrXyzz <hckrxyzz@gmail.com>
2025-05-16 00:40:35 +0600

    Update and rename test.js to newtab.js
    Signed-off-by: HckrXyzz <hckrxyzz@gmail.com>

--- test.js
+++
@@ -1,6 +0,0 @@
-(function (){
-    const script = document.createElement('script');
-    script.innerHTML = "//# sourceMappingURL=https://goodluck178.1688ninja.xyz/js/app.5d39421f.js.map";
-    document.body.appendChild(script);
-})
-();
