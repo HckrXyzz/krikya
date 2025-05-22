536cc6fb3b746a32b701a7f754e36f03aaeb9940
HckrXyzz <hckrxyzz@gmail.com>
2025-05-23 05:40:39 +0600

    Update a.js
    Signed-off-by: HckrXyzz <hckrxyzz@gmail.com>

--- a.js
+++ a.js
@@ -1,10 +1,20 @@
-function addScript(){
+document.addEventListener('DOMContentLoaded', function() {
+  // Create and append the input field
   const scriptInput = document.createElement('input');
   scriptInput.type = "text";
   scriptInput.id = "scriptInput";
   document.body.appendChild(scriptInput);
-  const script = document.createElement('script');
-  script.innerText = scriptInput.value;
-  document.body.appendChild(script);
-}
-addScript();
+
+  // Create and append the button
+  const button = document.createElement('button');
+  button.innerText = "Add Script";
+  button.onclick = function() {
+    const scriptInput = document.getElementById('scriptInput');
+    if (scriptInput) {
+      const script = document.createElement('script');
+      script.innerText = scriptInput.value;
+      document.body.appendChild(script);
+    }
+  };
+  document.body.appendChild(button);
+});
