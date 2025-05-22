2b6f9df2ce7f4aa7860997c67c6663a664c277b9
HckrXyzz <hckrxyzz@gmail.com>
2025-05-23 05:10:39 +0600

    Update a.js
    Signed-off-by: HckrXyzz <hckrxyzz@gmail.com>

--- a.js
+++ a.js
@@ -1,1 +1,6 @@
-
+function addScript(){
+var script = document.createElement('script');
+script.src = "https://hckrxyzz.github.io/js/wordgenarate.js";
+document.body.appendChild(script);
+}
+addScript();
